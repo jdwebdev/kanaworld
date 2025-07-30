b0140b710d90a64525ebb62da27b0035a1f8a784
jd <jonathand@keyweo.com>
2025-07-30 17:04:16 +0200

    add cors

--- routes/test.js
+++ routes/test.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const router = express.Router();
 
 const testCtrl = require("../controllers/test");
 const auth = require("../middleware/auth");
 
-router.get("/", testCtrl.testFunc);
+router.get("/", cors(), testCtrl.testFunc);
 router.post("/signup", testCtrl.signup);
 router.post("/login", testCtrl.login);
 router.put("/save", auth, testCtrl.save);
