60c1b9dd557b98b449dbd5b6a163d3d9bf54c1be
jd <jonathand@keyweo.com>
2025-07-31 16:45:16 +0200

    test with cors()

--- routes/test.js
+++ routes/test.js
@@ -1,13 +1,13 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const router = express.Router();
 
 const testCtrl = require("../controllers/test");
 const auth = require("../middleware/auth");
 
-router.get("/", testCtrl.testFunc);
-router.post("/signup", testCtrl.signup);
-router.post("/login", testCtrl.login);
-router.put("/save", auth, testCtrl.save);
+router.get("/", cors(), testCtrl.testFunc);
+router.post("/signup", cors(), testCtrl.signup);
+router.post("/login", cors(), testCtrl.login);
+router.put("/save", cors(), auth, testCtrl.save);
 
 module.exports = router;
